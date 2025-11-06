cb39f192d5f7586ef1383e15095a014c16334141
adambako <adambako@ADAMBAKO986C>
2025-11-06 12:44:49 +0200

    Add project files.

---
+++ UMFST.MIP.CarService/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity; // FONTOS: Ez most NEM Microsoft.EntityFrameworkCore
+
+namespace UMFST.MIP.CarService
+{
+    public class AppContext : DbContext
+    {
+
+        public AppContext() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<AppContext>());
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Diagnostic> Diagnostics { get; set; }
+        public DbSet<Test> Tests { get; set; }
+    }
+}
